f5d93b2247cfc0e062af53638e78a54ce37d378c
Sebastian Targosz <stargosz@sii.pl>
2021-04-15 14:35:30 +0200

    Updated task

--- src/JavaTask.java
+++ src/JavaTask.java
@@ -1,30 +1,10 @@
-import java.util.Scanner;
-public class JavaTask {
+public abstract class JavaTask{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number, counter;
-        System.out.println("How many films do you want to enter?");
-        number = scanner.nextInt();
-        String[] titles = new String[number];
-        String[] actors = new String[number];
-        String[] category = new String[number];
+        int TitlesLength, i;
+        TitlesLength = TitlesList.titles.length;
 
-        scanner.nextLine();
-        for (counter = 0; counter < number; counter++) {
-            System.out.print("Title " +(counter+1)+ ": ");
-            titles[counter] = scanner.nextLine();
-        }
-        for (counter = 0; counter < number; counter++){
-            System.out.print("Actor's name " +(counter+1)+ ": ");
-            actors[counter] = scanner.nextLine();
-        }
-        for (counter = 0; counter < number; counter++){
-            System.out.print("Category for movie " +(counter+1)+ ": ");
-            category[counter] = scanner.nextLine();
-        }
-        scanner.close();
-        for (counter = 0; counter < number; counter++) {
-            System.out.println("Should we cast " +actors[counter]+ " in "+titles[counter]+" and make it a "+category[counter]+ "?");
+        for (i = 0; i < TitlesLength; i++) {
+            System.out.println("Should we cast " + ActorsList.actors[i]+ " in "+ TitlesList.titles[i]+" and make it a "+ CategoriesList.category[i]+ " movie?");
         }
     }
 }
